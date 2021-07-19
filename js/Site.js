6726c56afcaa28b7f7435bb8e551b1ea606d83af
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2021-07-19 19:07:45 +0200

    Working Javascript

--- js/Site.js
+++ js/Site.js
@@ -1,4 +1,28 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World!");
+function getValues(){
+    let s = document.getElementById("inputString").value;
+
+    //alert(`pop my shit: ${s.length}`);
+    if(s.length == 0){
+        alert("Please enter content to rewind.");
+    }
+    else if(s.length == 1){
+        alert("A rewind on one character stays the same. Please enter more.");
+    }
+    else{
+        reverseString(s);
+    }
+}
+
+function reverseString(s){
+    let reverse = "";
+
+    for(let i = s.length-1; i >= 0; i--){
+        reverse += s.charAt(i);
+    }
+
+    displayHtml(reverse);
+}
+
+function displayHtml(result){
+    document.getElementById("results").innerHTML = result;
 }
